b1dc627e641c1e01a28c2494a7db772f16c0b03f
ayoung134 <ayou134@lsu.edu>
2026-02-26 11:28:35 -0600

    tried to add menu and hamburger as well as logo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // JS scripts placed here
+// const hamburger = document.querySelector('.hamburger');
+// const navLinks = document.querySelector('.nav-links');
+
+// hamburger .addEventListener('click', () => {
+//     if (menuOpen == false) {
+//         navLinks.computedStyleMap.display = "block";
+//         menuOpen = true;
+//     }
+//     else if (menuOpen == true) {
+//         navLinks.style.display = "none";
+//         menuOpen = false;
+//     }
+// })
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.getElementById('offcanvas-menu');
+
+menuToggle.addEventListener('click', () => {
+  const expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
+  menuToggle.setAttribute('aria-expanded',!expanded);
+  menu.classList.toggle('expanded');
+});
